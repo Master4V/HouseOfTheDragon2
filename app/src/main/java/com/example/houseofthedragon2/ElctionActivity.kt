bf94212b9e10dd69efc45c4014f79ec4a3d0922c
V Master <a2017dutuv@gmail.com>
2024-11-13 09:57:38 +0100

    Primer commit

---
+++ app/src/main/java/com/example/houseofthedragon2/ElctionActivity.kt
@@ -0,0 +1,24 @@
+package com.example.houseofthedragon2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class ElectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_elction)
+
+        //obtener una referencia a la etiqueta de la actividad
+        val tvGreeting = findViewById<TextView>(R.id.tvGreeting)
+
+        //recogemos el nombre del intent
+        val nombre = intent.getStringExtra("NAME")
+        //creamos la cadena del saludo
+        tvGreeting.apply {
+            //toma el valor de la cadena "greeting" y le incorpora el valor del nombre.
+            text = getString(R.string.greeting,nombre)
+        }
+
+    }
+}
